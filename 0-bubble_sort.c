2166ade74a012e985fa5891ff5adc5460bcf5704
ismael <spacingismael@gmail.com>
2024-02-16 00:31:13 +0100

    improving bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,12 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, tmp;
 
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
