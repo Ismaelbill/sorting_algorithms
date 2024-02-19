e62511f0e18fb1b934cab667d4e5ec276d58f8ce
ismael <spacingismael@gmail.com>
2024-02-19 10:04:35 +0100

    task 2 fixing

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,20 +11,27 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int min, tmp;
+	int min, tmp, swapped;
 	size_t i, j;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
+		swapped = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[min] > array[j])
+			{
 				min = j;
+				swapped = 1;
+			}
 		}
-		tmp = array[i];
-		array[i] = array[min];
-		array[min] = tmp;
-		print_array(array, size);
+		if (swapped)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
 	}
 }
